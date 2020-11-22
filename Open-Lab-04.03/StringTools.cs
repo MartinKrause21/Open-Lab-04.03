22a0e559672bc73135af7aa52720ad32f1d76712
MartinKrause21 <martin.21.krause@gmail.com>
2020-11-22 10:57:27 +0100

    Add function to remove first and last letter

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,7 +6,32 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+
+            string res = "";
+            int length = original.Length;
+
+            if (length == 0)
+
+                return original;
+
+            else if (length == 1)
+
+                return original;
+            else
+            {
+                for (int i = 1; i < (length - 1); i++)
+                {
+                    res += original[i];
+                }
+                return res;
+
+
+            }
+
+
+
         }
+
     }
 }
+
